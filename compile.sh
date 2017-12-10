6c134acc430e5b00a919ce7663dc05a53be60944
xophidia <xophidia@gmail.com>
2017-12-10 21:00:26 +0100

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nasm -f elf32 -o $1.o $1.asm
+
+echo "[+] compilation nasm"
+
+ld -m elf_i386 -o $1 $1.o
+
+echo "[+] Edition de liens"
+
+echo "[+] Shellcode ..."
+
+objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|sed 1d | cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+
+echo "[+] Done"
