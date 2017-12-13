3fb9f5a2620a22c90e3c8b0275d59d985be84586
xophidia <xophidia@gmail.com>
2017-12-13 20:30:23 +0100

    Update compile.sh
    Erreur dans la règle corrigée

--- compile.sh
+++ compile.sh
@@ -10,6 +10,6 @@ echo "[+] Edition de liens"
 
 echo "[+] Shellcode ..."
 
-objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|sed 1d | cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
 
 echo "[+] Done"
